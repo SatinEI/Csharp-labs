731605a33e9dccff11c6dbbc6b7f4ee7c6e577f8
nilaw <nilawyn202@gmail.com>
2026-02-17 19:59:08 +0900

    -

--- ConsoleApp1/DiscountCalculator.cs
+++ ConsoleApp1/DiscountCalculator.cs
@@ -1,32 +1,32 @@
 ﻿namespace ConsoleApp1
 {
     // Базовый класс для скидок
-    public abstract class DiscountStrategy
+    public abstract class Discount
     {
         public abstract decimal CalculateDiscount(decimal orderAmount);
         public abstract string CustomerType { get; }
     }
-    public class RegularDiscount : DiscountStrategy
+    public class RegularDiscount : Discount
     {
         public override string CustomerType => "Regular";
         public override decimal CalculateDiscount(decimal orderAmount) => orderAmount * 0.05m;
     }
-    public class PremiumDiscount : DiscountStrategy
+    public class PremiumDiscount : Discount
     {
         public override string CustomerType => "Premium";
         public override decimal CalculateDiscount(decimal orderAmount) => orderAmount * 0.10m;
     }
-    public class VIPDiscount : DiscountStrategy
+    public class VIPDiscount : Discount
     {
         public override string CustomerType => "VIP";
         public override decimal CalculateDiscount(decimal orderAmount) => orderAmount * 0.15m;
     }
-    public class StudentDiscount : DiscountStrategy
+    public class StudentDiscount : Discount
     {
         public override string CustomerType => "Student";
         public override decimal CalculateDiscount(decimal orderAmount) => orderAmount * 0.08m;
     }
-    public class SeniorDiscount : DiscountStrategy
+    public class SeniorDiscount : Discount
     {
         public override string CustomerType => "Senior";
         public override decimal CalculateDiscount(decimal orderAmount) => orderAmount * 0.07m;
@@ -39,33 +39,33 @@
 
 
     // Базовый класс для доставки
-    public abstract class ShippingStrategy
+    public abstract class Shipping
     {
         public abstract decimal CalculateShippingCost(decimal weight, string destination);
         public abstract string ShippingMethod { get; }
     }
-    public class StandardShipping : ShippingStrategy
+    public class StandardShipping : Shipping
     {
         public override string ShippingMethod => "Standard";
         public override decimal CalculateShippingCost(decimal weight, string destination)
             => 5.00m + (weight * 0.5m);
     }
 
-    public class ExpressShipping : ShippingStrategy
+    public class ExpressShipping : Shipping
     {
         public override string ShippingMethod => "Express";
         public override decimal CalculateShippingCost(decimal weight, string destination)
             => 15.00m + (weight * 1.0m);
     }
 
-    public class OvernightShipping : ShippingStrategy
+    public class OvernightShipping : Shipping
     {
         public override string ShippingMethod => "Overnight";
         public override decimal CalculateShippingCost(decimal weight, string destination)
             => 25.00m + (weight * 2.0m);
     }
 
-    public class InternationalShipping : ShippingStrategy
+    public class InternationalShipping : Shipping
     {
         public override string ShippingMethod => "International";
         public override decimal CalculateShippingCost(decimal weight, string destination)
